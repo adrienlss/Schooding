aac30706b8ad464c853c94fb0623454836de2bab
adrienlss <adrien.lassus@edu.ece.fr>
2025-10-01 14:15:19 +0200

    fix(ex2): créer route directe /ex2 pour exercice 2

---
+++ src/app/ex2/page.tsx
@@ -0,0 +1,10 @@
+export default function Ex2Page() {
+  return (
+    <iframe
+      src="/exam-ex2.html"
+      title="Exercice 2"
+      style={{ width: '100vw', height: '100vh', border: 'none', minWidth: '1280px' }}
+    />
+  );
+}
+
